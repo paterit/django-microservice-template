c5fb1f4fa541daab9f5319958f05b0202dc0db48
Wojtek Semik <wojtek.semik@gmail.com>
2018-03-24 00:41:33 +0100

    ELK upgraded. Version in copy_docker_images_to_machine.sh updated.

--- cicd/copy_docker_images_to_machine.sh
+++ cicd/copy_docker_images_to_machine.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-docker save python:3.6.1 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
-docker save alpine:3.5 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
-docker save nginx:1.11 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
-docker save sebp/elk:521 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
+docker save python:3.6.4 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
+docker save python:3.6.4-alpine3.7 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
+docker save alpine:3.7 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
+docker save nginx:1.13-alpine | pv | docker $(docker-machine config {{ project_name }}-cicd) load
+docker save sebp/elk:623 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
 docker save gliderlabs/logspout:v3.1 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
-docker save postgres:9.6 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
+docker save postgres:10.3-alpine | pv | docker $(docker-machine config {{ project_name }}-cicd) load
 
