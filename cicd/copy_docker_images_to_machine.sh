4e90b98ac12fd0aa27acf6885184de0a213753be
wojtek <wojtek.semik@gmail.com>
2019-03-26 17:52:54 +0100

    For django-postgresql updated from: 2.1.2-python3.6.6-node3.8 to: 2.1.7-python3.7.2-alpine3.9

--- cicd/copy_docker_images_to_machine.sh
+++ cicd/copy_docker_images_to_machine.sh
@@ -15,7 +15,7 @@ docker save kamon/grafana_graphite | pv | docker $(docker-machine config {{ proj
 docker save paterit/locustio:0.9.0-python3.6.6-alpine3.8 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
 docker save paterit/sphinx:1.5.3-python3.6.6-alpine3.8 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
 docker save paterit/node-behave:10.2-behave1.2.5-python3.6.5-alpine3.8 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
-docker save paterit/django-postgresql:2.1.2-python3.6.6-node3.8 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
+docker save paterit/django-postgresql:2.1.7-python3.7.2-alpine3.9 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
 docker save paterit/buildbot-worker-docker:1.1.0-docker18.06.1 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
 docker save portainer/portainer:1.20.2 | pv | docker $(docker-machine config {{ project_name }}-cicd) load
 
--- cicd/pull_base_docker_images.sh
+++ cicd/pull_base_docker_images.sh
@@ -13,6 +13,6 @@ docker pull kamon/grafana_graphite
 docker pull paterit/locustio:0.9.0-python3.6.6-alpine3.8
 docker pull paterit/sphinx:1.5.3-python3.6.6-alpine3.8
 docker pull paterit/node-behave:10.2-behave1.2.5-python3.6.5-alpine3.8
-docker pull paterit/django-postgresql:2.1.2-python3.6.6-node3.8
+docker pull paterit/django-postgresql:2.1.7-python3.7.2-alpine3.9
 docker pull paterit/buildbot-worker-docker:1.1.0-docker18.06.1
 docker pull portainer/portainer:1.20.2
