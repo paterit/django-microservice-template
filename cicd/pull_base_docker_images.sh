061c4aa5a849d83acb3d8d16ebc3131f276baa15
wojtek <wojtek.semik@gmail.com>
2019-04-01 22:32:17 +0200

    For django-postgresql updated from: 2.1.7-python3.7.2-alpine3.9 to: 2.1.8-python3.7.3-alpine3.9

--- cicd/pull_base_docker_images.sh
+++ cicd/pull_base_docker_images.sh
@@ -13,6 +13,6 @@ docker pull kamon/grafana_graphite
 docker pull paterit/locustio:0.11.0-3.7.2-alpine3.9
 docker pull paterit/sphinx:1.8.5-python3.7.2-alpine3.9
 docker pull paterit/node-behave:11.12-alpine-behave1.2.6-python3
-docker pull paterit/django-postgresql:2.1.7-python3.7.2-alpine3.9
+docker pull paterit/django-postgresql:2.1.8-python3.7.3-alpine3.9
 docker pull paterit/buildbot-worker-docker:2.1.0-docker18.06.3
 docker pull portainer/portainer:1.20.2
