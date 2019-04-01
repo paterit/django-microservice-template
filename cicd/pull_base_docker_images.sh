fc9f2288409e455b51644eb337c4a2baa9d9ccd7
wojtek <wojtek.semik@gmail.com>
2019-04-01 22:53:19 +0200

    For locustio updated from: 0.11.0-3.7.2-alpine3.9 to: 0.11.0-3.7.3-alpine3.9

--- cicd/pull_base_docker_images.sh
+++ cicd/pull_base_docker_images.sh
@@ -10,7 +10,7 @@ docker pull gliderlabs/logspout:v3.2.6
 docker pull postgres:11.2-alpine
 docker pull nicolargo/glances:v2.11.1
 docker pull kamon/grafana_graphite
-docker pull paterit/locustio:0.11.0-3.7.2-alpine3.9
+docker pull paterit/locustio:0.11.0-3.7.3-alpine3.9
 docker pull paterit/sphinx:1.8.5-python3.7.2-alpine3.9
 docker pull paterit/node-behave:11.12-alpine-behave1.2.6-python3
 docker pull paterit/django-postgresql:2.1.8-python3.7.3-alpine3.9
