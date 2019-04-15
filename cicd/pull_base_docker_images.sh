9a97438ef8132f7b0c3e66a9db51f785b77c6bb0
wojtek <wojtek.semik@gmail.com>
2019-04-15 18:23:01 +0200

    For elk updated from: 670 to: 700

--- cicd/pull_base_docker_images.sh
+++ cicd/pull_base_docker_images.sh
@@ -10,7 +10,7 @@ unset DOCKER_MACHINE_IP
 docker pull python:3.7.3-alpine3.9
 docker pull alpine:3.8
 docker pull nginx:1.15.10-alpine
-docker pull sebp/elk:670
+docker pull sebp/elk:700
 docker pull gliderlabs/logspout:v3.2.6
 docker pull postgres:11.2-alpine
 docker pull nicolargo/glances:v2.11.1
