e790e9f58eba2e58403ee4851ed984605a1e373e
wojtek <wojtek.semik@gmail.com>
2019-04-15 18:36:40 +0200

    For node-behave updated from: 11.13-alpine-behave1.2.6-python3 to: 11.14-alpine-behave1.2.6-python3

--- cicd/pull_base_docker_images.sh
+++ cicd/pull_base_docker_images.sh
@@ -17,7 +17,7 @@ docker pull nicolargo/glances:v2.11.1
 docker pull kamon/grafana_graphite
 docker pull paterit/locustio:0.11.0-3.7.3-alpine3.9
 docker pull paterit/sphinx:2.0.1-python3.7.3-alpine3.9
-docker pull paterit/node-behave:11.13-alpine-behave1.2.6-python3
+docker pull paterit/node-behave:11.14-alpine-behave1.2.6-python3
 docker pull paterit/django-postgresql:2.1.8-python3.7.3-alpine3.9
 docker pull paterit/buildbot-worker-docker:2.1.0-docker18.06.3
 docker pull portainer/portainer:1.20.2
