5817913c2a8d55d997a3441f38ae6824e0f54967
Wojciech Semik <wojtek.semik@gmail.com>
2020-10-26 21:40:52 +0100

    portainer updated from: 1.22.2 to: 1.24.1

--- cicd/pull_base_docker_images.sh
+++ cicd/pull_base_docker_images.sh
@@ -20,4 +20,4 @@ docker pull paterit/sphinx:2.2.1-python3.8.0-alpine3.10
 docker pull paterit/node-behave:14.14-alpine-behave1.2.6-python3
 docker pull paterit/django-postgresql:3.1.2-python3.9.0-alpine3.12
 docker pull paterit/buildbot-worker-docker:2.8.4-docker18.06.3
-docker pull portainer/portainer:1.22.2
+docker pull portainer/portainer:1.24.1
